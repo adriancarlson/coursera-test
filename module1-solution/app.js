6aa863d1d853d1a39ab9745c2b1277bbdbcbc6eb
Adrian Carlson <acarlson@goclg.com>
2019-01-16 22:05:17 -0600

    development 1/16/2019

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,10 +1,33 @@
 (function (){
-'use strict'
+'use strict';
 
-angular.module('LunchCheck',[])
+angular.module('LunchCheck', [])
     
-.controller('LunchCheckController', function (){
+.controller('LunchCheckController', LunchCheckController); 
+
+    LunchCheckController.$inject = ['$scope'];
     
-});
-    
+    function LunchCheckController($scope){
+        $scope.lunchMenu ="";
+        $scope.numberofItems = 0;
+        $scope.message = "";
+        
+        $scope.countItems = function (){
+            var countValue = ($scope.lunchMenu.split(',').length -1);
+        $scope.numberofItems = countValue;
+            
+            if ($scope.numberofItems == 0){
+                $scope.message = "Please enter data first";
+            } else if ($scope.numberofItems <= 3){
+                $scope.message = "Enjoy!";
+            } else {
+                $scope.message = "Too Much!";
+            }
+            
+            
+        };
+        
+        
+    }
 })();
+
